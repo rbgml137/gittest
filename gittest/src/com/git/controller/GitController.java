edeebf1ab7a07169e0bf02d529b8a52f76044fef
rbgml137 <rbgml137@naver.com>
2021-04-27 18:17:20 +0900

    #07 controller클래스, test()추가 -gh-

---
+++ gittest/src/com/git/controller/GitController.java
@@ -0,0 +1,8 @@
+package com.git.controller;
+
+public class GitController {
+	public void test() {
+		System.out.println("추가한클래스 매소드");
+	}
+	
+}
--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,10 +1,16 @@
 package com.git.main;
 
+import java.util.Date;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		int su=4;
-		Scanner sc=new Scanner(System.in);
+		System.out.println("안녕 git~ 반가워 오랜만이야");
+		
+		Date today=new Date();
+		System.out.println(today);
+		
+		
 	}
 	
 
