13c1772cd7ed7d3fa764f19b1360c999c67dafc9
rbgml137 <rbgml137@naver.com>
2021-04-27 18:11:39 +0900

    #06 처음부터다시시작 날림 -gh-

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -3,12 +3,8 @@ package com.git.main;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("안녕 git~ 반가워 오랜만이야");
-		
-		Date today=new Date();
-		System.out.println(today);
-		
-		
+		int su=4;
+		Scanner sc=new Scanner(System.in);
 	}
 	
 
