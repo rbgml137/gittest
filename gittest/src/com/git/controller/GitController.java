f9f184abc47949323c29243efd70ef0d56bb6cce
rbgml137 <rbgml137@naver.com>
2021-04-27 20:37:18 +0900

    #08 test2매소드추가 -gh-

--- gittest/src/com/git/controller/GitController.java
+++ gittest/src/com/git/controller/GitController.java
@@ -1,8 +1,14 @@
 package com.git.controller;
 
 public class GitController {
+	
 	public void test() {
 		System.out.println("추가한클래스 매소드");
+	}
+		
+	
+	public void test2() {
+		System.out.println("원격으로 저장하자!");		
 	}
 	
 }
