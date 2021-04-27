7563f01cd4c41c3dc40bb1cbf69a3a880c7dba7a
rbgml137 <rbgml137@naver.com>
2021-04-27 18:03:34 +0900

    #04 오늘날짜출력문추가 -bs-

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("안녕 git~ 반가워 오랜만이야");
 		
+		Date today=new Date();
+		System.out.println(today);
+		
+		
 	}
 	
 
